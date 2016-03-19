7198a13ba795720fb9c6dccf704776db11fbda1d
airuep22 <ava7ar.e@gmail.com>
2016-03-19 10:06:59 +0100

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Paweł Tarszewski on 19.03.2016.
+//  Copyright © 2016 Paweł Tarszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
