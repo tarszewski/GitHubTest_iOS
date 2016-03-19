e749327c45ee4a05a49096248c8c41c6fc8c10b0
tarszewski <pawel.tarszewski@icloud.com>
2016-03-19 10:23:49 +0100

    Update ViewController.swift

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "no hello"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
